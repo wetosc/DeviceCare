57761e832c5d36961eed355e20b1f081cc3f1da9
Eugenius <eugen.cernei@gmail.com>
2017-06-22 10:50:06 +0300

    Updated to Android Studio 3

---
+++ DeviceCare/app/src/main/java/com/labs/fi141/devicecare/db/Table.kt
@@ -0,0 +1,33 @@
+package com.labs.fi141.devicecare.db
+
+import java.util.HashMap
+
+/**
+ * Created by eugenius on 3/19/17.
+ */
+
+class Table {
+    var name: String? = null
+    var fields: HashMap<String, String>? = null
+
+    val CREATE: String
+        get() {
+            var str = "create table ${name} ("
+            for (key in fields!!.keys) {
+                str += "${key} ${fields!![key]},"
+            }
+            str.subSequence(0, str.length - 1)
+            str += ");"
+            return str
+        }
+
+    val DROP: String
+        get() = "drop table if exists ${name};"
+
+    val SELECT: String
+        get() = "select * from ${name}"
+
+    fun getSELECT(paramName: String): String {
+        return "select * from ${name} where ${paramName} == ?"
+    }
+}
